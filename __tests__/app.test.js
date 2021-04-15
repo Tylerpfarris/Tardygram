4d0e75cde4448b8ff774d5438bcab80b9cb9a67e
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-15 11:12:20 -0700

    Attempts at setting up a post route and model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,23 @@ describe('Tardygram routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  // needs to update to reflect auth routes
+  it('adds a new post', async () => {
+    const response = await request(app)
+      .post('/api/v1/posts')
+      .send({
+        userName: 'devon_wolf',
+        photoUrl: 'some.image.url',
+        caption: 'look at this amazing image',
+        tags: ['image', 'great', 'image', 'hashtag CRUD']
+      });
+
+    expect(response.body).toEqual({
+      userName: 'thatUser',
+      photoUrl: 'some.image.url',
+      caption: 'look at this amazing image',
+      tags: ['image', 'great', 'image', 'hashtag CRUD']
+    });
+  });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,24 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+
 
 CREATE TABLE users (
     github_user_name TEXT NOT NULL PRIMARY KEY,
     github_avatar_url TEXT NOT NULL
 );
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY NOT NULL,
+    post_by TEXT NOT NULL REFERENCES users(github_user_name),
+    photo_url TEXT NOT NULL,
+    caption TEXT,
+    tags TEXT[]
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY NOT NULL,
+    comment_by TEXT NOT NULL REFERENCES users(github_user_name),
+    post INTEGER NOT NULL REFERENCES posts(id),
+    comment TEXT NOT NULL
+)
