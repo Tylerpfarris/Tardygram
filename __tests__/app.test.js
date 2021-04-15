0efafbe4751e4e30245f386947132655e394f7a8
Tyler Farris <Tylerpfarris@gmail.com>
2021-04-15 12:56:35 -0700

    wrote get all post form a user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,11 +2,29 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
+const Post = require('../lib/models/Post');
 
 describe('Tardygram routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  beforeEach( async () => {
+    await User.insert({
+      userName: 'devon_wolf',
+      avatarUrl: 'some.image.url'
+    })
+  });
+  beforeEach( async () => {
+    await Post.insert({
+      userName: 'devon_wolf',
+      photoUrl: 'some.image.url',
+      caption: 'look at this amazing image',
+      tags: ['image', 'great', 'image', 'hashtag CRUD']
+    })
+  });
+
 
   // needs to update to reflect auth routes
   it('adds a new post', async () => {
@@ -20,10 +38,22 @@ describe('Tardygram routes', () => {
       });
 
     expect(response.body).toEqual({
-      userName: 'thatUser',
+      userName: 'devon_wolf',
       photoUrl: 'some.image.url',
       caption: 'look at this amazing image',
       tags: ['image', 'great', 'image', 'hashtag CRUD']
     });
+  });
+
+  it('gets all posts from a user', async () => {
+    const response = await request(app)
+      .get('/api/v1/posts')
+    
+    expect(response.body).toEqual([{
+      userName: 'devon_wolf',
+      photoUrl: 'some.image.url',
+      caption: 'look at this amazing image',
+      tags: ['image', 'great', 'image', 'hashtag CRUD']
+    }])
   });
 });
